d921c188f0b445b2862c138eec4280318c3d6531
julien97300 <applewhitejulien@gmail.com>
2025-07-20 17:44:02 -0400

    Add .next/static/chunks/src_app_page_tsx_07f5d755._.js 1753047841942

---
+++ .next/static/chunks/src_app_page_tsx_07f5d755._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_01aba41a._.js",
+  "static/chunks/node_modules_8c2fbe25._.js"
+],
+    source: "dynamic"
+});
